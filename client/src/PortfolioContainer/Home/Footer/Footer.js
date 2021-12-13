2ab2232b6b8f018c85b18d67edbd17776be96c46
Doven <doven.obertinca@gmail.com>
2021-12-13 16:56:21 +0100

    footer bg photo etc

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -3,8 +3,10 @@ import "./Footer.css"
 
 export default function Footer() {
     return (
-        <div>
-            
+        <div className="footer-container">
+            <div className="footer-parent">
+                {/* <img src={require("../../../assets/Home/shape-bg.png").default} alt="no internet connection" /> */}
+            </div>
         </div>
     )
 }
