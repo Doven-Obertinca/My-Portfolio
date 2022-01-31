f92a4a3f65a1e7d80975b6ba025dec891b531497
Doven <doven.obertinca@gmail.com>
2022-01-31 15:32:08 +0100

    some commits

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
+import ScrollService from '../../utilities/ScrollService'
+import Animation from '../../utilities/Animation'
+
+export default function AboutMe() {
+  return <div></div>;
+}
