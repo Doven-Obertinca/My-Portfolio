3df0e9c898fa3f5bf394a64c5fc491f6eb34ca13
Doven <doven.obertinca@gmail.com>
2022-02-03 18:32:17 +0100

    about me component

--- client/src/utilities/ScreenHeading/ScreenHeading.js
+++ client/src/utilities/ScreenHeading/ScreenHeading.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./ScreenHeading.css"
 
 export default function ScreenHeading(props) {
-  return <div className="heading-container">
+  return (<div className="heading-container">
       <div className="screen-heading">
           <span>{props.title}</span>
       </div>
@@ -11,13 +11,20 @@ export default function ScreenHeading(props) {
                 <div className="screen-sub-heading">
                     <span>{props.subHeading}</span>
                 </div>
-            ):<div className="heading-seperator">
+            ):(
+                <div></div>
+            )}
+
+
+            <div className="heading-seperator">
                 <div className="seperator-line">
                     <div className="seperator-blob">
                         <div></div>
                     </div>
                 </div>
             </div>
-        }
-  </div>;
+            
+        
+  </div>
+  );
 }
